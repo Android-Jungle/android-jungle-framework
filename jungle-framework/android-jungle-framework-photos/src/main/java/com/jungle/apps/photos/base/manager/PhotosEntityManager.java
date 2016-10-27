6b5dede12c3ccd943314dc1de4ee24b6ae8ab8a4
arnozhang <zyfgood12@163.com>
2016-10-27 14:12:55 +0800

    fix module. step 2. add Volley for Http requests.

--- jungle-framework/android-jungle-framework-photos/src/main/java/com/jungle/apps/photos/base/manager/PhotosEntityManager.java
+++ jungle-framework/android-jungle-framework-photos/src/main/java/com/jungle/apps/photos/base/manager/PhotosEntityManager.java
@@ -5,8 +5,10 @@ import com.jungle.apps.photos.module.favorite.data.pic.FavoriteEntity;
 import com.jungle.apps.photos.module.favorite.data.tag.FavoriteTagEntity;
 import com.jungle.base.app.AppCore;
 import com.jungle.base.manager.AppManager;
+import com.jungle.base.utils.FileUtils;
 import com.jungle.simpleorm.supporter.ORMDatabaseListener;
 import com.jungle.simpleorm.supporter.ORMSupporter;
+import com.jungle.simpleorm.supporter.SQLiteORMSupporter;
 import com.jungle.simpleorm.supporter.SimpleORMDatabaseListener;
 
 public class PhotosEntityManager implements AppManager {
@@ -14,6 +16,9 @@ public class PhotosEntityManager implements AppManager {
     public static PhotosEntityManager getInstance() {
         return AppCore.getInstance().getManager(PhotosEntityManager.class);
     }
+
+    private static final int DB_VERSION = 1;
+    private ORMSupporter mGlobalSupporter;
 
 
     protected ORMDatabaseListener getDBListener() {
@@ -37,16 +42,18 @@ public class PhotosEntityManager implements AppManager {
     }
 
     public ORMSupporter getGlobalORMSupporter() {
-        return null;
+        return mGlobalSupporter;
     }
 
     @Override
     public void onCreate() {
-
+        String filePath = FileUtils.getDatabaseFilePath("entity.db");
+        mGlobalSupporter = new SQLiteORMSupporter(
+                AppCore.getApplicationContext(), filePath, DB_VERSION, getDBListener());
     }
 
     @Override
     public void onTerminate() {
-
+        mGlobalSupporter.close();
     }
 }
